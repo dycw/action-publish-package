ca04e076954c8b67758986de5d0b9ed585e8d259
Derek Wan <d.wan@icloud.com>
2025-12-18 16:04:11 +0900

    2025-12-18 16:04:11 (Thu)  > DW-Mac  > derekwan

--- src/uv_publish/cli.py
+++ src/uv_publish/cli.py
@@ -15,13 +15,17 @@ from uv_publish.settings import Settings
 @command(**CONTEXT_SETTINGS_HELP_OPTION_NAMES)
 @click_options(Settings, "app", show_envvars_in_help=True)
 def _main(settings: Settings, /) -> None:
+    basic_config(obj=LOGGER)
+    LOGGER.info(
+        """\
+Running version %s with settings:
+%s""",
+        __version__,
+        pretty_repr(settings),
+    )
     if settings.dry_run:
         LOGGER.info("Dry run; exiting...")
         return
-    basic_config(obj=LOGGER)
-    LOGGER.info(
-        "Running version %s with settings:\n%s...", __version__, pretty_repr(settings)
-    )
     uv_publish(
         username=settings.username,
         password=settings.password,
--- src/uv_publish/lib.py
+++ src/uv_publish/lib.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 
 from utilities.tempfile import TemporaryDirectory
 
+from uv_publish import __version__
 from uv_publish.logging import LOGGER
 from uv_publish.settings import SETTINGS
 
@@ -20,6 +21,22 @@ def uv_publish(
     trusted_publishing: bool = SETTINGS.trusted_publishing,
     native_tls: bool = SETTINGS.native_tls,
 ) -> None:
+    LOGGER.info(
+        """\
+Running %s with settings:
+ - username           = %s
+ - password           = %s
+ - publish_url        = %s
+ - trusted_publishing = %s
+ - native_tls         = %s
+ """,
+        __version__,
+        username,
+        password,
+        publish_url,
+        trusted_publishing,
+        native_tls,
+    )
     with TemporaryDirectory() as temp:
         _log_run("uv", "build", "--out-dir", str(temp), "--wheel", "--clear")
         _log_run(
